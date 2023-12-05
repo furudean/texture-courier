85032411835302559c4c4e3fd70d27151ff4f12e
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2023-12-05 09:13:46 -0700

    stray handler

--- src/texture_courier/__init__.py
+++ src/texture_courier/__init__.py
@@ -4,10 +4,6 @@ from .api import TextureCache, Texture  # noqa
 from .find import list_texture_cache  # noqa
 
 
-def _sigint_handler(signum, frame):
-    exit(130)
-
-
 if __name__ == "__main__":
     try:
         main()
