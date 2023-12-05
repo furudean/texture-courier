b766a77b776022ebb0a7997b723fe5c8e18b5517
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2023-12-04 22:16:21 -0700

    more repr nits

--- src/texture_courier/api.py
+++ src/texture_courier/api.py
@@ -15,14 +15,16 @@ def loads_bytes_io(p: Path) -> BytesIO:
     return BytesIO(p.read_bytes())
 
 
-def format_bytes(size: float) -> tuple[float, str]:
+def format_bytes(size: float) -> str:
     power = 2**10
     n = 0
     power_labels = {0: "bytes", 1: "KB", 2: "MB", 3: "GB", 4: "TB"}
+
     while size > power:
         size /= power
         n += 1
-    return size, power_labels[n]
+
+    return f"{int(size)} {power_labels[n]}"
 
 
 class Texture:
@@ -43,8 +45,8 @@ class Texture:
             setattr(self, key, value)
 
     def __repr__(self) -> str:
-        size, unit = format_bytes(self.image_size)
-        return f"<Texture {self.uuid}, {self.time}, {int(size)} {unit}>"
+        size = format_bytes(self.image_size) if self.is_empty else "empty"
+        return f"<Texture {self.uuid}, {self.time}, {size}>"
 
     @property
     def is_empty(self) -> bool:
@@ -81,7 +83,13 @@ class TextureCache:
         return iter(self.textures.values())
 
     def __repr__(self) -> str:
-        return f"<TextureCache {self.cache_dir.resolve()}, {self.header['entry_count']} entries>"
+        total_size = sum(texture.image_size for texture in self)
+
+        return (
+            f"<TextureCache {self.cache_dir.resolve()}, "
+            f"{self.header['entry_count']} entries, "
+            f"{format_bytes(total_size)}>"
+        )
 
     def __get_read_bytes(self, i: int, entry: core.Entry) -> Callable[[], bytes]:
         def read_bytes() -> bytes:
